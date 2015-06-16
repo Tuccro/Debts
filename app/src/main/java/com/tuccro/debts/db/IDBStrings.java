c32fb8bae064e3ba2c04433fb2e6fd3b12559485
Tuccro <tuccro@yahoo.com>
2015-06-16 20:47:48 +0300

    Added IDBStrings Interface

--- app/src/main/java/com/tuccro/debts/db/IDBStrings.java
+++ app/src/main/java/com/tuccro/debts/db/IDBStrings.java
@@ -3,6 +3,11 @@ package com.tuccro.debts.db;
 
 public interface IDBStrings {
 
-    String createDB = "";
+    String DB_CREATE_TABLE_PEOPLE = "CREATE TABLE `people` (\n" +
+            "\t`_id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\n" +
+            "\t`name`\tTEXT NOT NULL UNIQUE,\n" +
+            "\t`phone`\tTEXT,\n" +
+            "\t`date_of_create`\tTEXT NOT NULL\n" +
+            ");";
 
 }
