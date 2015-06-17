6dabeab3d124cd4f05544813d1165cb67957f2ea
Tuccro <tuccro@yahoo.com>
2015-06-18 01:18:58 +0300

    Added DB create Strings

--- app/src/main/java/com/tuccro/debts/db/IDBStrings.java
+++ app/src/main/java/com/tuccro/debts/db/IDBStrings.java
@@ -1,6 +1,5 @@
 package com.tuccro.debts.db;
 
-
 public interface IDBStrings {
 
     String DB_CREATE_TABLE_PEOPLE = "CREATE TABLE `people` (\n" +
@@ -12,12 +11,15 @@ public interface IDBStrings {
 
     String DB_CREATE_TABLE_MONEY = "CREATE TABLE `money` (\n" +
             "\t`_id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\n" +
+            "\t`people_id`\tINTEGER NOT NULL,\n" +
             "\t`currency_id`\tINTEGER NOT NULL,\n" +
             "\t`sum`\tREAL NOT NULL,\n" +
             "\t`note`\tTEXT,\n" +
             "\t`date_add`\tTEXT NOT NULL,\n" +
             "\t`date_begin`\tTEXT,\n" +
             "\t`date_end`\tTEXT\n" +
+            "\tFOREIGN KEY (people_id) REFERENCES people(_id) ON DELETE CASCADE\n" +
+            "\tFOREIGN KEY (currency_id) REFERENCES currency(_id) ON DELETE CASCADE" +
             ");";
 
     String DB_CREATE_TABLE_CURRENCY = "CREATE TABLE `currency` (\n" +
