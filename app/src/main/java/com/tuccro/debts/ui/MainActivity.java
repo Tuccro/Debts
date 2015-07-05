ea91bafbf4225cb7cdc1d763a506187dc0a59fef
Tuccro <tuccro@yahoo.com>
2015-07-05 23:56:27 +0300

    changed strings

--- app/src/main/java/com/tuccro/debts/ui/MainActivity.java
+++ app/src/main/java/com/tuccro/debts/ui/MainActivity.java
@@ -115,8 +115,6 @@ public class MainActivity extends AppCompatActivity implements MoneyFragment.OnM
                     return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
                     return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
         }
